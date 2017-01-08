3cffbf2df0e6489703ca2fdc4ae6e9a355ad57a1
Tony Galmiche <tony.galmiche@infosaone.com>
2017-01-08 16:34:52 +0100

    add report_liste_article

--- __openerp__.py
+++ __openerp__.py
@@ -86,6 +86,7 @@ Module principal du projet Plastigray
         "views/report_bon_transfert.xml",
         "views/report_purchaseorder.xml",
         "views/report_ar_commande.xml",
+        "views/report_liste_article.xml",
         "views/report.xml",
         "report/stock_bloquer_lot.xml",
         "report/stock_debloquer_lot.xml",
