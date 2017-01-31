280db96e2e9c0e329023010d641df61ad0adacf9
Tony Galmiche <tony.galmiche@infosaone.com>
2017-01-31 14:41:11 +0100

    pour export des stocks

--- report/is_stock_quant.py
+++ report/is_stock_quant.py
@@ -11,6 +11,8 @@ class is_stock_quant(models.Model):
     _auto = False
 
     product_id         = fields.Many2one('product.template', 'Article')
+    code_pg            = fields.Char('Code PG')
+    designation        = fields.Char('Désignation')
     gestionnaire_id    = fields.Many2one('is.gestionnaire' , 'Gestionnaire')
     category_id        = fields.Many2one('is.category'     , 'Catégorie')
     moule              = fields.Char('Moule')
@@ -19,6 +21,7 @@ class is_stock_quant(models.Model):
     ref_client         = fields.Char('Référence Client')
     ref_fournisseur    = fields.Char('Référence Fournisseur')
     location_id        = fields.Many2one('stock.location'     , 'Emplacement')
+    emplacement        = fields.Char('Emplacement')
     lot                = fields.Char('Lot')
     lot_fournisseur    = fields.Char('Lot fournisseur')
     quantite           = fields.Float('Quantité')
@@ -34,6 +37,8 @@ class is_stock_quant(models.Model):
                 select
                     max(isq.id) as id,
                     isq.product_id, 
+                    isq.code_pg,
+                    isq.designation,
                     isq.gestionnaire_id,
                     isq.category_id,
                     isq.moule,
@@ -41,6 +46,7 @@ class is_stock_quant(models.Model):
                     isq.ref_client,
                     isq.ref_fournisseur,
                     isq.location_id,
+                    isq.emplacement,
                     isq.lot,
                     isq.lot_fournisseur,
                     sum(isq.quantite) as quantite, 
@@ -52,6 +58,8 @@ class is_stock_quant(models.Model):
                     select 
                         sq.id                  id,
                         pt.id                  product_id, 
+                        pt.is_code             code_pg,
+                        pt.name                designation,
                         pt.is_gestionnaire_id  gestionnaire_id,
                         pt.is_category_id      category_id,
                         im.name                moule,
@@ -59,16 +67,18 @@ class is_stock_quant(models.Model):
                         pt.is_ref_client       ref_client,
                         pt.is_ref_fournisseur  ref_fournisseur,
                         sq.location_id         location_id,
+                        sl.name                emplacement,
                         spl.name               lot,
                         spl.is_lot_fournisseur lot_fournisseur,
-                        sq.qty                quantite, 
-                        pt.uom_id             uom_id,
-                        in_date               date_entree,
+                        sq.qty                 quantite, 
+                        pt.uom_id              uom_id,
+                        in_date                date_entree,
                         (   select client_id 
                             from is_product_client ipc
                             where ipc.product_id=pt.id and ipc.client_defaut='t' limit 1) client_id
                     from stock_quant sq inner join product_product            pp on sq.product_id=pp.id
                                         inner join product_template           pt on pp.product_tmpl_id=pt.id
+                                        inner join stock_location             sl on sq.location_id=sl.id
                                         left outer join stock_production_lot spl on sq.lot_id=spl.id
                                         left outer join is_mold               im on pt.is_mold_id=im.id
                                         left outer join is_dossierf           id on pt.is_dossierf_id=id.id
@@ -76,6 +86,8 @@ class is_stock_quant(models.Model):
 
                 group by
                     isq.product_id, 
+                    isq.code_pg,
+                    isq.designation,
                     isq.gestionnaire_id,
                     isq.category_id,
                     isq.moule,
@@ -83,6 +95,7 @@ class is_stock_quant(models.Model):
                     isq.ref_client,
                     isq.ref_fournisseur,
                     isq.location_id,
+                    isq.emplacement,
                     isq.lot,
                     isq.lot_fournisseur,
                     isq.uom_id,
