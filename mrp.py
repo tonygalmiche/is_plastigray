6e1feb9f1205db2a87a04a459a3aa6d2d7747194
Tony Galmiche <tony.galmiche@infosaone.com>
2017-03-30 18:25:43 +0200

    add 3 en mod

--- mrp.py
+++ mrp.py
@@ -128,7 +128,19 @@ class mrp_routing_workcenter(models.Model):
 
     hour_nbr         = fields.Float("Nombre d'heures"      , digits=(12,6), method=True, type='float', store=True, readonly=True, compute='_hour_nbr')
     is_nb_secondes   = fields.Float("Nombre de secondes"   , digits=(12,2), required=False, help="Nombre de secondes")
-    is_nb_mod        = fields.Selection([('0.25', '0.25'), ('0.3', '0.3'), ('0.5', '0.5'), ('0.75', '0.75'), ('1', '1'), ('1.5', '1.5'), ('2', '2')], 'Nombre de MOD', help='Donnée utilisée en particlier pour le planning')
+    is_nb_mod        = fields.Selection([
+        ('0.25', '0.25'), 
+        ('0.3' , '0.3'), 
+        ('0.5' , '0.5'), 
+        ('0.75', '0.75'), 
+        ('1'   , '1'), 
+        ('1.25', '1.25'), 
+        ('1.5' , '1.5'), 
+        ('1.75', '1.75'), 
+        ('2'   , '2'),
+        ('2.5' , '2.5'),
+        ('3'   , '3'),
+    ], 'Nombre de MOD', help='Donnée utilisée en particlier pour le planning')
 
 
 
