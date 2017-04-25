0d01d47744860a3c6ff3fa9dd2554a0446c2144f
Tony Galmiche <tony.galmiche@infosaone.com>
2017-04-25 14:23:09 +0200

    add is_adresse_ip

--- res_users.py
+++ res_users.py
@@ -1,5 +1,7 @@
-from openerp import models,fields,api
+from openerp import models,fields,api, SUPERUSER_ID
 from openerp.tools.translate import _
+
+from openerp.http import request
 
 
 class is_service(models.Model):
@@ -13,6 +15,21 @@ class res_users(models.Model):
     _inherit = "res.users"
 
     is_service_id = fields.Many2one('is.service', 'Service')
+    is_adresse_ip = fields.Char('Adresse IP')
+
+
+    def _login(self, db, login, password):
+        """Permet d'ajouter l'adresse IP de la personne qui se connecte
+        cela est utilise par les programmes externes"""
+
+        user_id = super(res_users, self)._login(db, login, password)
+        ip=request.httprequest.environ['REMOTE_ADDR'] 
+        cr = self.pool.cursor()
+        cr.autocommit(True)
+        if user_id and ip:
+            res=cr.execute("UPDATE res_users SET is_adresse_ip='"+str(ip)+"' WHERE id="+str(user_id))
+            cr.close()
+        return user_id
 
 
 class res_groups(models.Model):
