451941eeb5fbae03f1a09e0650b4c1b2f062d1ee
Chetan <scrdhaduk@gmail.com>
2017-05-05 14:40:34 +0530

    From gabarit, instrument, plaquette remove security group and add in form view.

--- is_instrument_mesure.py
+++ is_instrument_mesure.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models,fields,api
+from openerp import models, fields, api
 from openerp.tools.translate import _
 from openerp.exceptions import Warning
 from datetime import datetime
 
 class is_instrument_mesure(models.Model):
-    _name='is.instrument.mesure'
-    _order='code_pg'
+    _name = 'is.instrument.mesure'
+    _order = 'code_pg'
 
-    
+    @api.depends('famille_id', 'fabriquant_id')
+    def _compute_periodicite(self):
+        for obj in self:
+            obj.periodicite = ''
+
     code_pg = fields.Char("Code PG")
     designation = fields.Char("Désignation")
-    famille_id = fields.Many2one("is.famille.instrument","Famille")
-    fabriquant_id = fields.Many2one("res.partner","Fabricant")
+    famille_id = fields.Many2one("is.famille.instrument", "Famille")
+    fabriquant_id = fields.Many2one("res.partner", "Fabricant")
     num_serie = fields.Char("N° de série")
     date_reception = fields.Date("Date de réception")
     type = fields.Char("Type")
@@ -22,26 +26,26 @@ class is_instrument_mesure(models.Model):
     type_boolean = fields.Boolean('Is Type?', default=False)
     etendue_boolean = fields.Boolean('Is Etendue?', default=False)
     resolution_boolean = fields.Boolean('Is Résolution?', default=False)
-    site_id = fields.Many2one("is.emplacement.outillage","Site d'affectation")
+    site_id = fields.Many2one("is.emplacement.outillage", "Site d'affectation")
     lieu_stockage = fields.Char("Lieu de stockage")
     service_affecte = fields.Char("Personne/Service auquel est affecté l'instrument")
     
     
-    frequence = fields.Selection([('intensive','utilisation quotidienne'),('moyenne','utilisation plusieurs jours par semaine'),
-                                  ('faible','utilisation 1 fois par semaine ou moins')],"Fréquence")
-    periodicite = fields.Char("Périodicité")
+    frequence = fields.Selection([('intensive', 'utilisation quotidienne'), ('moyenne', 'utilisation plusieurs jours par semaine'),
+                                  ('faible', 'utilisation 1 fois par semaine ou moins')], "Fréquence")
+    periodicite = fields.Char("Périodicité", store=True, compute='_compute_periodicite')
     
     
-    controle = fields.Selection([('creation','Création'),('verification','Vérification'),('maintenance','Maintenance'),
-                                 ('arret','Arrêt'),('visuel','Visuel'),('etalonnage','étalonnage')], string="Contrôle")
+    controle = fields.Selection([('creation', 'Création'), ('verification', 'Vérification'), ('maintenance', 'Maintenance'),
+                                 ('arret', 'Arrêt'), ('visuel', 'Visuel'), ('etalonnage', 'étalonnage')], string="Contrôle")
     cause_arret = fields.Char("Cause arrêt")
     commentaire = fields.Char("Commentaire")
     date_controle = fields.Date("Date du contrôle")
-    organisme_controleur = fields.Selection([('interne','Interne'),('externe','Externe')],"Organisme contrôleur")
-    fournisseur_id = fields.Many2one('res.partner','Fournisseur')
-    classe = fields.Selection([('0','0'),('1','1'),('2','2'),('3','3'),('na','/NA')],"Classe")
+    organisme_controleur = fields.Selection([('interne', 'Interne'), ('externe', 'Externe')], "Organisme contrôleur")
+    fournisseur_id = fields.Many2one('res.partner', 'Fournisseur')
+    classe = fields.Selection([('0', '0'), ('1', '1'), ('2', '2'), ('3', '3'), ('na', '/NA')], "Classe")
     classe_boolean = fields.Boolean('Is Classe?', default=False)
-    etat_conformite = fields.Selection([('conforme','Conforme'),('non_conforme','Non Conforme')],string="Etat de la conformité")
+    etat_conformite = fields.Selection([('conforme', 'Conforme'), ('non_conforme', 'Non Conforme')], string="Etat de la conformité")
     rapport_de_controle = fields.Binary("Rapport de contrôle")
     
     @api.onchange('famille_id')
@@ -53,7 +57,7 @@ class is_instrument_mesure(models.Model):
         
         
 class is_famille_instrument(models.Model):
-    _name='is.famille.instrument'
+    _name = 'is.famille.instrument'
     
     name = fields.Char("Nom de la famille")
     intensive = fields.Char("INTENSIVE (fréquence f >= 1 fois / jour) en mois")
