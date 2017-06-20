02bcf322ec572e55ea7cf33ecbf0a12ba1e219f5
Tony Galmiche <tony.galmiche@infosaone.com>
2017-06-20 14:00:09 +0200

    amélioration performances is_stock_move (x2)

--- report/is_stock_move.py
+++ report/is_stock_move.py
@@ -37,19 +37,19 @@ class is_stock_move(models.Model):
             CREATE OR REPLACE view is_stock_move AS (
 
                 select 
-                        row_number() over(order by sm.id ) as id,
-                        sm.id                              as move_id,
-                        sm.date                            as date,
-                        sm.product_id                      as product_id, 
-                        ic.name                            as category,
-                        COALESCE(im.name,id.name)          as mold,
-                        COALESCE(spt.name,sm.src)          as type_mv,
-                        COALESCE(spt.name,sp.name,sm.name) as name,
-                        sm.picking_id                      as picking_id,
-                        sm.purchase_line_id,
-                        sm.raw_material_production_id,
-                        sm.production_id,
-                        sm.is_sale_line_id,
+                        row_number() over(order by sm.id )  as id,
+                        sm.id                               as move_id,
+                        sm2.date                            as date,
+                        sm2.product_id                      as product_id, 
+                        ic.name                             as category,
+                        COALESCE(im.name,id.name)           as mold,
+                        COALESCE(spt.name,sm.src)           as type_mv,
+                        COALESCE(spt.name,sp.name,sm2.name) as name,
+                        sm2.picking_id                      as picking_id,
+                        sm2.purchase_line_id,
+                        sm2.raw_material_production_id,
+                        sm2.production_id,
+                        sm2.is_sale_line_id,
                         sm.lot_id                          as lot_id,
                         spl.is_lot_fournisseur             as lot_fournisseur,
                         sm.qty                             as qty,
@@ -60,22 +60,10 @@ class is_stock_move(models.Model):
 
                     select 
                         sm.id,
-                        sm.date,
-                        sm.product_id, 
-                        sm.picking_type_id,
-                        sm.picking_id,
-                        sm.purchase_line_id,
-                        sm.raw_material_production_id,
-                        sm.production_id,
-                        sm.is_sale_line_id,
-                        sm.name,
-                        sm.origin,
                         sq.lot_id           as lot_id,
                         sum(sq.qty)         as qty,
                         sl1.name            as src,
-                        sl2.name            as dest,
-                        sm.write_date,
-                        sm.write_uid
+                        sl2.name            as dest
                     from stock_move sm inner join stock_location        sl1 on sm.location_id=sl1.id
                                        inner join stock_location        sl2 on sm.location_dest_id=sl2.id
                                        left outer join stock_quant_move_rel sqmr on sm.id=sqmr.move_id
@@ -83,42 +71,18 @@ class is_stock_move(models.Model):
                     where sm.state='done' and sl2.usage='internal' 
                     group by 
                         sm.id,
-                        sm.date,
-                        sm.product_id, 
-                        sm.picking_type_id,
-                        sm.picking_id,
-                        sm.purchase_line_id,
-                        sm.raw_material_production_id,
-                        sm.production_id,
-                        sm.is_sale_line_id,
-                        sm.name,
-                        sm.origin,
                         sq.lot_id,
                         sl1.name,
-                        sl2.name,
-                        sm.write_date,
-                        sm.write_uid 
+                        sl2.name
 
                     union
 
                     select 
                         sm.id,
-                        sm.date,
-                        sm.product_id, 
-                        sm.picking_type_id,
-                        sm.picking_id,
-                        sm.purchase_line_id,
-                        sm.raw_material_production_id,
-                        sm.production_id,
-                        sm.is_sale_line_id,
-                        sm.name,
-                        sm.origin,
                         sq.lot_id           as lot_id,
                         -sum(sq.qty)        as qty,
                         sl1.name            as dest,
-                        sl2.name            as src,
-                        sm.write_date,
-                        sm.write_uid
+                        sl2.name            as src
                     from stock_move sm inner join stock_location        sl1 on sm.location_dest_id=sl1.id
                                        inner join stock_location        sl2 on sm.location_id=sl2.id
                                        left outer join stock_quant_move_rel sqmr on sm.id=sqmr.move_id
@@ -126,33 +90,22 @@ class is_stock_move(models.Model):
                     where sm.state='done' and sl2.usage='internal' 
                     group by 
                         sm.id,
-                        sm.date,
-                        sm.product_id, 
-                        sm.picking_type_id,
-                        sm.picking_id,
-                        sm.purchase_line_id,
-                        sm.raw_material_production_id,
-                        sm.production_id,
-                        sm.is_sale_line_id,
-                        sm.name,
-                        sm.origin,
                         sq.lot_id,
                         sl1.name,
-                        sl2.name,
-                        sm.write_date,
-                        sm.write_uid 
+                        sl2.name
 
 
-                ) as sm inner join product_product            pp on sm.product_id=pp.id
+                ) sm    inner join stock_move                sm2 on sm.id=sm2.id           
+                        inner join product_product            pp on sm2.product_id=pp.id
                         inner join product_template           pt on pp.product_tmpl_id=pt.id
-                        inner join res_users                  ru on sm.write_uid=ru.id
-                        left outer join stock_picking_type   spt on sm.picking_type_id=spt.id
-                        left outer join stock_picking         sp on sm.picking_id=sp.id
+                        inner join res_users                  ru on sm2.write_uid=ru.id
+                        left outer join stock_picking_type   spt on sm2.picking_type_id=spt.id
+                        left outer join stock_picking         sp on sm2.picking_id=sp.id
                         left outer join is_category           ic on pt.is_category_id=ic.id
                         left outer join is_mold               im on pt.is_mold_id=im.id
                         left outer join is_dossierf           id on pt.is_dossierf_id=id.id
                         left outer join stock_production_lot spl on sm.lot_id=spl.id
-                order by sm.date desc, sm.id
+                order by sm2.date desc, sm2.id
             );
 
         """)
