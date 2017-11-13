e96fdab1e4544d7d3b8c03140c1c593ee6399fe1
Tony Galmiche <tony.galmiche@infosaone.com>
2017-11-13 08:59:09 +0100

    add is_demande_achat_invest

--- __init__.py
+++ __init__.py
@@ -41,6 +41,7 @@ import is_prechauffeur
 import is_demande_achat
 import is_demande_achat_serie
 import is_demande_achat_fg
+import is_demande_achat_invest
 import is_facture_pk
 import is_moyen_fabrication
 import is_moyen_fabrication_autre
