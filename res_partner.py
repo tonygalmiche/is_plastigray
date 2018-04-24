143301d6dee2e8ef3fd5dcf281f881b43957920c
Emmanuel Garette <egarette@cadoles.com>
2018-04-24 16:21:26 +0200

    [1422] ajoute le lieu dans la fiche client/fournisseur

--- res_partner.py
+++ res_partner.py
@@ -183,6 +183,7 @@ class res_partner(models.Model):
     is_adr_groupe           = fields.Char('Code auxiliaire comptable', help="Code auxiliaire comptable de l'adresse groupe pour la comptabilité")
     is_cofor                = fields.Char('N° fournisseur (COFOR)', help="Notre code fourniseur chez le client")
     is_incoterm             = fields.Many2one('stock.incoterms', "Incoterm")
+    is_lieu                 = fields.Char("Lieu")
     is_escompte             = fields.Many2one('is.escompte', "Escompte")
     is_type_reglement       = fields.Many2one('account.journal', u'Type règlement', domain=[('type', 'in', ['bank','cash'])])
     is_num_siret            = fields.Char(u'N° de SIRET')
