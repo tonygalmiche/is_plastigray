38908bbb1a84b05ba0c153afb6ca42f32b5df828
Emmanuel Garette <egarette@cadoles.com>
2018-12-19 14:53:26 +0100

    [1505] version imprimable du tableau GEFBOX avec séparateur en jaune

--- __openerp__.py
+++ __openerp__.py
@@ -155,6 +155,7 @@ Module principal du projet Plastigray
         "views/report_emballage.xml",
         "views/report_ligne_reception_tree.xml",
         'views/report_is_reach.xml',
+        'views/report_is_livraison_gefbox_tree.xml',
         "views/report.xml",
         "report/stock_bloquer_lot.xml",
         "report/stock_debloquer_lot.xml",
